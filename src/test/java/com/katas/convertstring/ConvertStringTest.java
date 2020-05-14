7e1476473b4ddf496155e3d3d44da7f6d40fe023
Jesus A. Dominguez <jesus.domin@gmail.com>
2020-05-14 19:46:57 +0200

    Remove redundant test

--- src/test/java/com/katas/convertstring/ConvertStringTest.java
+++ src/test/java/com/katas/convertstring/ConvertStringTest.java
@@ -12,30 +12,23 @@ class ConvertStringTest {
     }
 
     @Test
-    void shouldConvertCamelCaseExceptFirstWord() {
-        String input = "the-stealth-Warrior";
-        System.out.println("input: " + input);
-        assertEquals("theStealthWarrior", ConvertString.toCamelCase(input));
-    }
-
-    @Test
-    void shouldConvertCamelCaseAndKeepFirstWordCapitalization() {
-        String input = "The-stealth-warrior";
-        System.out.println("input: " + input);
-        assertEquals("TheStealthWarrior", ConvertString.toCamelCase(input));
-    }
-
-    @Test
     public void testSomeUnderscoreLowerStart() {
         String input = "the_Stealth_Warrior";
-        System.out.println("input: "+input);
+
         assertEquals("theStealthWarrior", ConvertString.toCamelCase(input));
     }
 
     @Test
     public void testSomeDashLowerStart() {
         String input = "the-Stealth-Warrior";
-        System.out.println("input: "+input);
+
         assertEquals("theStealthWarrior", ConvertString.toCamelCase(input));
+    }
+
+    @Test
+    void shouldConvertCamelCaseAndKeepFirstWordCapitalization() {
+        String input = "The-stealth-warrior";
+
+        assertEquals("TheStealthWarrior", ConvertString.toCamelCase(input));
     }
 }
